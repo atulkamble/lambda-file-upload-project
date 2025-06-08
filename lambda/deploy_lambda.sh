c5b90f4d621d44286a1496260e9eb3c8f75d232e
Atul Kamble <atul_kamble@hotmail.com>
2025-06-08 11:43:51 +0530

    Update deploy_lambda.sh

--- lambda/deploy_lambda.sh
+++ lambda/deploy_lambda.sh
@@ -6,7 +6,7 @@ zip function.zip lambda_function.py
 aws lambda create-function \
   --function-name uploadHandler \
   --runtime python3.12 \
-  --role arn:aws:iam::YOUR_ACCOUNT_ID:role/LambdaExecutionRole \
+  --role arn:aws:iam::535002879962:role/LambdaExecutionRole \
   --handler lambda_function.lambda_handler \
   --zip-file fileb://function.zip \
   --region us-east-1
